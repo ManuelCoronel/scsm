75b77b0f43ddfa770d486c18d2acb6534be468fc
Sachica <rojassachicadf@ufps.edu.co>
2021-06-08 20:50:34 -0500

    Cambios en microcurriculos

--- src/java/control/ControladorMicrocurriculo.java
+++ src/java/control/ControladorMicrocurriculo.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
